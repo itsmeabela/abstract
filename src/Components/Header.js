0767a9df7d4782afa2b0c56c6fce9bbbb0433f32
itsmeabela <abelrobel7@gmail.com>
2023-02-11 23:55:54 +0300

    done assets

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+//images
+import logo from "../Assets/header-logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="contents">
+        <div className="logo_content">
+          <img src={logo} alt="logo" />
+          <span> | Help Center</span>
+        </div>
+
+        <div className="signup_content">
+          <button className="btn-1">Submit a request</button>
+          <button className="btn-2">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
